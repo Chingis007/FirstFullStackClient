91d6948468d0c813f21bea94f7d9c1468d8ac59c
Chingis007 <karaulnekerell@gmail.com>
2023-02-19 12:29:18 +0200

    ;;;

---
+++ src/pages/ChangePassword.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+function ChangePassword() {
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  let navigate = useNavigate();
+
+  const changePassword = () => {
+    axios
+      .put(
+        "http://localhost:3001/auth/changepassword",
+        { oldPassword: oldPassword, newPassword: newPassword },
+        {
+          headers: {
+            accessToken: localStorage.getItem("accessToken"),
+          },
+        }
+      )
+      .then((response) => {
+        if (response.data.error) {
+          alert(response.data.error);
+        } else {
+          navigate("/login");
+        }
+      });
+  };
+  return (
+    <div>
+      <h1>Wanna Change?</h1>
+      <input
+        type="text"
+        placeholder="Old Password..."
+        onChange={(event) => {
+          setOldPassword(event.target.value);
+        }}
+      />
+      <input
+        type="text"
+        placeholder="New Password..."
+        onChange={(event) => {
+          setNewPassword(event.target.value);
+        }}
+      />
+      <button onClick={changePassword}>Save Changes</button>
+    </div>
+  );
+}
+
+export default ChangePassword;
---
+++ src/pages/Post.js
@@ -0,0 +1,171 @@
+import React from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import axious from "axios";
+import { useEffect, useState, useContext } from "react";
+import { AuthContext } from "../helpers/AuthContext";
+
+function Post() {
+  let navigate = useNavigate();
+  let { id } = useParams();
+  const [postObject, setPostObject] = useState({});
+  const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState("");
+  const { authState } = useContext(AuthContext);
+
+  useEffect(() => {
+    axious.get(`http://localhost:3001/posts/byId/${id}`).then((response) => {
+      setPostObject(response.data);
+    });
+
+    axious.get(`http://localhost:3001/comments/${id}`).then((response) => {
+      setComments(response.data);
+    });
+  }, []);
+
+  const addComment = (data) => {
+    axious
+      .post(
+        "http://localhost:3001/comments",
+        { commentBody: newComment, PostId: id },
+        {
+          headers: {
+            accessToken: localStorage.getItem("accessToken"),
+          },
+        }
+      )
+      .then((response) => {
+        if (response.data.error) {
+          alert(response.data.error);
+        } else {
+          axious
+            .get(`http://localhost:3001/comments/${id}`)
+            .then((response) => {
+              setComments(response.data);
+            });
+          // const commentToAdd = {
+          //   commentBody: newComment,
+          //   username: response.data.username,
+          // };
+          // setComments([...comments, commentToAdd]);
+          // setNewComment("");
+        }
+      });
+  };
+
+  const deleteComment = (id) => {
+    axious
+      .delete(`http://localhost:3001/comments/${id}`, {
+        headers: { accessToken: localStorage.getItem("accessToken") },
+      })
+      .then(() => {
+        setComments(
+          comments.filter((val) => {
+            return val.id !== id;
+          })
+        );
+      });
+  };
+
+  const deletePost = () => {
+    axious
+      .delete(`http://localhost:3001/posts/${id}`, {
+        headers: { accessToken: localStorage.getItem("accessToken") },
+      })
+      .then(() => {
+        navigate("/");
+      });
+  };
+  const editPost = (option) => {
+    if (option === "title") {
+      let newTitle = prompt("Enter new Title:");
+      axious.put(
+        "http://localhost:3001/posts/title",
+        { newTitle: newTitle, id: id },
+        { headers: { accessToken: localStorage.getItem("accessToken") } }
+      );
+      setPostObject({ ...postObject, title: newTitle });
+    } else {
+      let newPostText = prompt("Enter new Text:");
+      axious.put(
+        "http://localhost:3001/posts/postText",
+        { newPostText: newPostText, id: id },
+        { headers: { accessToken: localStorage.getItem("accessToken") } }
+      );
+      setPostObject({ ...postObject, postText: newPostText });
+    }
+  };
+  return (
+    <div className="postPage">
+      <div className="leftSide">
+        <div className="post" id="individual">
+          <div
+            className="title"
+            onClick={() => {
+              if (authState.username === postObject.username) {
+                editPost("title");
+              }
+            }}
+          >
+            {postObject.title}
+          </div>
+          <div
+            className="body"
+            onClick={() => {
+              if (authState.username === postObject.username) {
+                editPost("body");
+              }
+            }}
+          >
+            {postObject.postText}
+          </div>
+          <div className="footer">
+            {postObject.username}
+            {authState.username === postObject.username && (
+              <button
+                onClick={() => {
+                  deletePost(postObject.id);
+                }}
+              >
+                Delete Post
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="rightSide">
+        <div className="addCommentContainer">
+          <input
+            type="text"
+            placeholder="Comment..."
+            value={newComment}
+            onChange={(event) => {
+              setNewComment(event.target.value);
+            }}
+          />
+          <button onClick={addComment}>Add Comments</button>
+        </div>
+        <div className="listOfComments">
+          {comments.map((comment, key) => {
+            return (
+              <div className="comment">
+                {comment.commentBody}
+                <label> Username: {comment.username}</label>
+                {authState.username === comment.username && (
+                  <button
+                    onClick={() => {
+                      deleteComment(comment.id);
+                    }}
+                  >
+                    X
+                  </button>
+                )}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Post;
